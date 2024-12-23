705e3a4decf12274c18667f5dd8e40ae3573bf3b
arielkoh02 <s2227491@ed.ac.uk>
2024-12-23 10:12:48 +0000

    glm-2.2

---
+++ glm-2.2/torunglm-2.2.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+
+# Load libraries
+library(glmtools)
+library(rLakeAnalyzer)
+setwd("C:/Users/ASUS/Desktop/UoE/Dissertation/dissertation/glm-2.4.1")
+
+# Install and run glmGUI
+#devtools::install_github("jackpmcd/glmgui")
+glmgui::glmGUI()
+
+run_glm()
+   
